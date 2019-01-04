4c0f2190158462a8f230141aca4c667887a70c22
Alexandre Müller Júnior <alexandremj@protonmail.com>
2019-01-04 17:49:51 -0200

    Add help text

--- harmonic_field.py
+++ harmonic_field.py
@@ -1,4 +1,5 @@
-from notes import Notes 
+from notes import Notes
+import pprint
 
 class HarmonicField():
 
@@ -7,13 +8,32 @@ class HarmonicField():
     
     """ Generates the Major Scale of the given tonic
         and returns a dictionary containing the notes """
-    def major_natural_scale(self):
+    def major_scale(self):
         scale = {}
-        scale['first_degree'] = self.tonic
-        scale['second_degree'] = self.tonic + 2
-        scale['third_degree'] = self.tonic + 4
-        scale['fourth_degree'] = self.tonic + 5
-        scale['fifth_degree'] = self.tonic + 7
-        scale['sixth_degree'] = self.tonic + 9
-        scale['seventh_degree'] = self.tonic + 11
+        scale['first_degree'] = self.tonic.name
+        scale['second_degree'] = (self.tonic + 2).name
+        scale['third_degree'] = (self.tonic + 4).name
+        scale['fourth_degree'] = (self.tonic + 5).name
+        scale['fifth_degree'] = (self.tonic + 7).name
+        scale['sixth_degree'] = (self.tonic + 9).name
+        scale['seventh_degree'] = (self.tonic + 11).name
+
+        print(scale)
+
+        return scale
+
+    """ Generates the Minor Natural Scale of the given tonic
+        and returns a dictionary containing the notes """
+    def minor_natural_scale(self):
+        scale = {}
+        scale['first_degree'] = self.tonic.name
+        scale['second_degree'] = (self.tonic + 2).name
+        scale['third_degree'] = (self.tonic + 3).name
+        scale['fourth_degree'] = (self.tonic + 5).name
+        scale['fifth_degree'] = (self.tonic + 7).name
+        scale['sixth_degree'] = (self.tonic + 8).name
+        scale['seventh_degree'] = (self.tonic + 10).name
+
+        print(scale)
+
         return scale
--- main.py
+++ main.py
@@ -2,18 +2,32 @@ import sys
 from notes import Notes
 from harmonic_field import HarmonicField
 
+HELP_TEXT = """ usage: python main.py tonic scale_1 scale_2 ... scale_n
+                where:
+                    tonic - the tonic chord of the scales
+                    
+                    scales currently supported:
+                        mj - major scale
+                        min - minor natural scale"""
+
 def main():
     if len(sys.argv) < 2:
-        print('help text - no arguments')
-        exit()
-
-    if sys.argv[1].casefold() == 'help':
-        print("help text")
+        print(HELP_TEXT)
         exit()
     
     tonic = Notes[sys.argv[1]]
     field = HarmonicField(tonic)
-    print(field.major_natural_scale())
 
-if __name__ == "__main__":
+    for command in sys.argv[2:]:
+        command = command.casefold()
+
+        if command == 'help':
+            print(HELP_TEXT)
+            exit()
+        elif command == 'mj':
+            field.major_scale()
+        elif command == 'min':
+            field.minor_natural_scale()
+
+if __name__ == '__main__':
     main()
